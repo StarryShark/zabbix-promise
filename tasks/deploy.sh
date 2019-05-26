29e410392a0926c304cf036328c1d05297904c2b
Sumit Goel <sumit@goel.pw>
2019-05-25 22:31:58 -0700

    Update travis build

--- tasks/deploy.sh
+++ tasks/deploy.sh
@@ -3,9 +3,10 @@
 set -ev
 
 if [ -z "$TRAVIS_TAG" ]; then
-  git config --local user.name "${github_username:?}"
-  git config --local user.email "${github_email:?}"
-  TRAVIS_TAG=$(jq .version package.json | tr -d '"')
-  export TRAVIS_TAG
-  git tag "v$TRAVIS_TAG"
+  # git config --local user.name "${github_username:?}"
+  # git config --local user.email "${github_email:?}"
+  # TRAVIS_TAG=$(jq .version package.json | tr -d '"')
+  # export TRAVIS_TAG
+  # git tag "v$TRAVIS_TAG"
+  echo 'Hey there!'
 fi
